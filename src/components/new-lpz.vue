5a8b1582c53a30ff08b88f6fffad33c036f7ce7c
Evgen <eok8177@gmail.com>
2018-01-18 22:02:37 +0200

    newLpz method; add axios package

---
+++ src/components/new-lpz.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="onSubmitted">
+      <div>
+          <label for="name">Name</label>
+          <input type="text" id="name" v-model="lpzName">
+      </div>
+      <button class="btn" type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+      return {
+          lpzName: ''
+      }
+  },
+  methods: {
+      onSubmitted() {
+          axios.post('http://lara.local/api/lpz', {name: this.lpzName})
+            .then(
+                (response) => console.log(response)
+            )
+            .catch(
+                (error) => console.log(error)
+            );
+      }
+  }
+}
+</script>
+
